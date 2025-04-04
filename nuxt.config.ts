810a4a092b5de25ae25ef27fab8ddad361eee24e
kevinlogan94 <kevinmlogan7@gmail.com>
2025-04-03 20:13:01 -0400

    configurations for css/scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/icon']
+  app: {
+    head: {
+      title: 'Ask Dwight',
+      meta: [
+        { name: 'description', content: 'Ask Dwight - Your sales AI assistant' }
+      ]
+    }
+  },
+  css: [
+    '~/assets/main.scss'
+  ],
+  modules: ['@nuxt/ui', '@nuxt/icon'],
+  compatibilityDate: '2024-11-01'
 })
