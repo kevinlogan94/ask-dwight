ad2b9ec27950cde16d5adb22c9efba2178541984
kevinlogan94 <kevinmlogan7@gmail.com>
2025-04-13 10:52:25 -0400

    add branding to comingsoonform and add favicon

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,14 +1,7 @@
 <template>
   <nav class="fixed w-full bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-800">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="flex justify-between h-16">
-        <div class="flex">
-          <NuxtLink to="/" class="flex items-center">
-            <img src="/favicon.ico" alt="Ask Dwight Logo" class="h-8 w-auto" />
-            <span class="ml-3 font-bold text-xl text-gray-900 dark:text-white">Ask Dwight</span>
-          </NuxtLink>
-        </div>
-        
+    <div class="px-4 sm:px-6 lg:px-8">
+      <div class="flex justify-end h-16">
         <div class="flex items-center">
           <ColorModeButton />
         </div>
