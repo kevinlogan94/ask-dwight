3e6fdfc296d90679fd87b65a0fa5aaf64fdc00ae
kevinlogan94 <kevinmlogan7@gmail.com>
2025-05-15 19:28:09 -0400

    add main component

--- app.vue
+++ app.vue
@@ -2,8 +2,10 @@
   <UApp :ui="{ tooltip: true }">
     <div class="bg-gradient-to-b min-h-screen to-gray-400 dark:from-gray-900 dark:to-gray-800">
       <Navbar />
-      <NuxtRouteAnnouncer />
-      <NuxtPage />
+      <UMain>
+        <NuxtRouteAnnouncer />
+        <NuxtPage />
+      </UMain>
     </div>
   </UApp>
 </template>
