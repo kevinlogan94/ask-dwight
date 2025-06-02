c2400988aa2dd511ca01bdb6ccd19f18f7ad4e31
kevinlogan94 <kevinmlogan7@gmail.com>
2025-06-02 08:55:38 -0400

    Add helpers

--- models/generic.ts
+++ models/generic.ts
@@ -1,3 +1,5 @@
 export enum SupabaseFunctions {
-    STORE_LOGS = "store-application-logs"
+    STORE_LOGS = "store-application-logs",
+    CHAT_CONVERSATIONS = "chat-conversations",
+    RESPONSE_CONVERSATIONS = "response-conversations"
 }
---
+++ supabase/functions/chat-conversations/helpers.ts
@@ -0,0 +1,7 @@
+export function organizeSuggestions(suggestions: string): string[] {
+    return suggestions
+      .split("\n")
+      .map((s) => s.trim())
+      .filter((s) => s.length > 0);
+  }
+  
