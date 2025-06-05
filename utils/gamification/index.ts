e963b0679464e2e0819e287dc4095c4981f45bff
kevinlogan94 <kevinmlogan7@gmail.com>
2025-06-04 23:16:45 -0400

    remove unnecessary logs

--- utils/gamification/index.ts
+++ utils/gamification/index.ts
@@ -46,14 +46,8 @@ export function getTotalTimeSaved(conversations: Conversation[]): number {
     const userMessages = conversation.messages.filter((msg) => msg.sender === "user");
 
     for (const message of userMessages) {
-      const { timeSaved, category } = organizePromptInfo(message.content);
+      const { timeSaved } = organizePromptInfo(message.content);
       totalTimeSaved += timeSaved;
-
-      // mainly worried about prompts not matching and falling into the OTHER category.
-      // todo: Remove this later
-      if (category === SalesActivityCategory.OTHER && totalTimeSaved < 20) {
-        console.log("category", category, message.content);
-      }
     }
   }
 
